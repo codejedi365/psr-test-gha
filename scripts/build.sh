11ec20049770f840cc3b3c36c6b59ab04eeb90e5
codejedi365 <codejedi365@gmail.com>
2024-05-12 20:16:20 -0400

    test: `fix/build-cmd-env` branch

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,6 +6,14 @@ PROJECT_DIR="$(dirname "$(dirname "${BASH_SOURCE[0]}")")"
 
 cd "$PROJECT_DIR" || exit 1
 
+if [ "${CI_JOB_ID:="-"}" = "-" ]; then
+    echo >&2 "Error: CI_JOB_ID is missing from environment" && exit 1
+fi
+
+if [ "${APPLICATION_NAME:="-"}" = "-" ]; then
+    echo >&2 "Error: APPLICATION_NAME is missing from environment" && exit 1
+fi
+
 if [ -n "$PSR_DOCKER_GITHUB_ACTION" ]; then
     # PSR's Docker Action does not have the build requirements installed or access to
     # a virtual env so must re-install inside the container environment
